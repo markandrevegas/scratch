a4e1f0a2349ed56a5a0bd4964e9f574fdc13ad6e
markandre <mark@haandvaerker.dk>
2025-08-09 13:48:50 +0200

    nuxt image

--- components/AudioPlayer.vue
+++ components/AudioPlayer.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-  import { PlaySolid, PauseSolid } from '@iconoir/vue'
   import { onMounted } from 'vue'
   import { useRadio } from '../composables/useScratchRadio'
   const { status, isPlaying, play, pause, song, fetchScratchRadio } = useRadio()
@@ -10,20 +9,23 @@
 </script>
 <template>
   <div>
-    <div class="w-[420px] flex justify-around"></div>
-    <div class="grid grid-cols-4 gap-2 w-[420px] p-2 rounded-xl border border-slate-300 mx-auto">
-      <div class="col-span-1 rounded-lg bg-sky-500"></div>
-      <div class="col-span-2 rounded-lg bg-sky-500">
-        <div v-if="status">
-          <span>Title: {{ song.title }}</span>
-          <br/>
-          <span>Artist: {{ song.artist }}</span>
-          <p>Listeners: {{ status.icestats?.source?.listeners }}</p>
-        </div>
-        <PlaySolid v-if="!isPlaying" class="h-[2rem] w-[2rem]" @click="play" />
-        <PauseSolid v-if="isPlaying" class="h-[2rem] w-[2rem]" @click="pause" />
+    <div class="grid grid-cols-4 gap-2 w-[420px] p-2 h-24 mx-auto">
+      <div class="rounded-lg bg-sky-500">
+        <NuxtImg v-if="song.image" :src="song.image" alt="Song Cover" width="64" height="64" class="h-16 w-16 rounded-lg object-cover" />
+        <NuxtImg v-else provider="placehold" src="64x64" alt="Default Cover" width="64" height="64" class="h-16 w-16 rounded-lg object-cover" />
       </div>
-      <div class="col-span-1 rounded-lg bg-sky-500"></div>
+      <div class="col-span-2 rounded-lg text-xs flex flex-col justify-center items-start">
+        <div v-if="status">
+          <span class="uppercase font-inter font-bold">{{ song.title }}</span>
+          <br/>
+          <span class="text-slate-400">{{ song.artist }}</span>
+          <!-- <p>Listeners: {{ status.icestats?.source?.listeners }}</p> -->
+        </div>
+      </div>
+      <div class="rounded-full border border-slate-400">
+        <span v-if="!isPlaying" @click="play">Play</span>
+        <span v-if="isPlaying" @click="pause">Pause</span>
+      </div>
     </div>
   </div>
 </template>
