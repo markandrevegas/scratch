ef3bc7f6f0d58b2cdfeeb8c022169a7bc403db3a
markandre <mark@haandvaerker.dk>
2025-08-17 15:59:29 +0200

    color classes

--- composables/useUnsplash.ts
+++ composables/useUnsplash.ts
@@ -6,16 +6,8 @@ export const useUnsplash = () => {
     return await $fetch('/api/unsplash/random', { query: params })
   }
 
-  // --- Helpers ---
-  /**
-   * Convert a full Unsplash image URL into a relative one for <NuxtImg>
-   */
-  const formatSrc = (url: string) => {
-    return url.replace('https://images.unsplash.com/', '')
-  }
 
   return {
-    getRandomPhoto,
-    formatSrc
+    getRandomPhoto
   }
 }
