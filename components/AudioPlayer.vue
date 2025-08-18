c7869047ecf573a88dff73975deac77a43d6a015
markandre <mark@haandvaerker.dk>
2025-08-18 21:30:45 +0200

    worked on album art lookup

--- components/AudioPlayer.vue
+++ components/AudioPlayer.vue
@@ -1,30 +1,32 @@
 <script setup lang="ts">
-  import { onMounted } from 'vue'
+  import { ref, onMounted } from 'vue'
   import { useRadio } from '../composables/useScratchRadio'
-  import { useUnsplash } from '../composables/useUnsplash'
+  // import { useUnsplash } from '../composables/useUnsplash'
+  import { useAlbumArt } from '../composables/useAlbumArt'
   const { status, isPlaying, play, pause, song, fetchScratchRadio } = useRadio()
-  const { getRandomPhoto } = useUnsplash()
-  const { getAlbumArt } = useAlbumArt()
+  // const { getRandomPhoto } = useUnsplash()
+  // const { getAlbumArt } = useAlbumArt()
 
-  const artist = 'Cher'
-  const track = 'Believe'
+  // const artist = 'Cher'
+  // const track = 'Believe'
   // const albumArt = ref<string | null>(null)
   // Fetch photos
-  const random = await getRandomPhoto({ orientation: 'landscape' })
-  const albumArt = ref<string | null>(null)
+  // const random = await getRandomPhoto({ orientation: 'landscape' })
+  // const albumArt = ref<string | null>(null)
   // const search = await searchPhotos('mountains', { per_page: 6 })
 
   onMounted(async () => {
     fetchScratchRadio()
-    albumArt.value = await getAlbumArt(artist, track)
-    console.log(albumArt.value)
+    // albumArt.value = await getAlbumArt(artist, track)
+    // console.log(albumArt.value)
   })
 </script>
 <template>
   <div>
     <div class="font-sans grid grid-cols-4 w-[420px] my-24 h-16 mx-auto align-center gap-4">
       <div class="flex justify-center items-center">
-        <NuxtImg provider="ipx" :src="random.urls.thumb" class="w-full h-full rounded-lg shadow" />
+        <NuxtImg v-if="status && song.art" provider="ipx" :src="song.art" class="w-full h-full rounded-lg shadow" />
+        <NuxtImg v-else src="64x64" provider="placehold" height="64" width="64" class="w-full h-full rounded-lg shadow" />
       </div>
       <div class="col-span-2 rounded-lg flex flex-col justify-center items-start">
         
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -54,6 +54,8 @@ export default defineNuxtConfig({
     domains: ['placehold.co']
   },
   runtimeConfig: {
+    spotifyClientId: process.env.SPOTIFY_CLIENT_ID,
+    spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET,
     unsplashAccessKey: process.env.UNSPLASH_ACCESS_KEY,
     public: {
       lastfmApiKey: process.env.LASTFM_API_KEY,
