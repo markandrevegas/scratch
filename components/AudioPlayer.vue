f98f2901410c36a6895a9b64ebec87b893d31d61
markandre <mark@haandvaerker.dk>
2025-08-23 10:25:13 +0200

    worked on layout

--- components/AudioPlayer.vue
+++ components/AudioPlayer.vue
@@ -35,20 +35,20 @@
 </script>
 <template>
   <div>
-    <div class="shadow-lg border border-slate-100 grid grid-cols-3 rounded-l-lg w-96">
+    <div class="shadow-lg border border-slate-100 grid grid-cols-4 rounded-l-lg">
       <div class="flex justify-center items-center w-full">
         <NuxtImg v-if="song.art" provider="ipx" :src="song.art" class="w-full h-full rounded-l-lg shadow" />
         <NuxtImg v-else src="48x48" provider="placehold" class="w-full h-full rounded-l-lg shadow" />
       </div>
-      <div class="col-span-2 rounded-lg flex justify-start items-start">
-        <div class="flex flex-col justify-between h-full w-full pt-4 pr-4 pb-2 pl-4">
+      <div class="col-span-3 rounded-lg flex justify-start items-start">
+        <div class="flex flex-col justify-between h-full w-full pt-4 pr-4 pb-2 pl-4 gap-2">
           <div>
             <p class="text-sm font-semibold text-abyssal dark:text-slate-200 mb-[2px]">{{ song.title }}</p>
-            <p class="text-xs font-medium text-cyan-800 opacity-60 dark:text-slate-300 leading-none">{{ song.artist }}</p>
+            <p class="text-xs font-medium text-slate-600 opacity-60 dark:text-slate-300 leading-none">{{ song.artist }}</p>
           </div>
           <div class="w-full flex justify-between items-center">
-            <progress :value="elapsedTime" max="120" class="progress-bar w-5/6 h-[2px] rounded-full overflow-hidden appearance-none" />
-            <span class="text-xs text-cyan-800/60">{{ formatTime(elapsedTime) }}</span>
+            <progress :value="elapsedTime" max="3600" class="progress-bar w-5/6 h-[2px] rounded-full overflow-hidden appearance-none" />
+            <span class="text-xs text-slate-600/60">{{ formatTime(elapsedTime) }}</span>
           </div>
           <div class="h-8 w-full grid grid-cols-4 text-abyssal hover:text-abyssal dark:hover:text-slate-200 rounded-full flex items-center justify-between hover:cursor-pointer duration-500" @click="isPlaying ? pause() : play()">
             <Transition name="fade" mode="out-in">
