3c6a026bd3939d2d8a627d66383cf69a48d28e71
markandrevegss <markandrevega@me.com>
2025-08-23 12:02:04 +0200

    colormodetoggle fixes.

--- pages/ScratchRadio.vue
+++ pages/ScratchRadio.vue
@@ -5,7 +5,6 @@
 <template>
   <div class="dark:bg-abyssal h-screen flex flex-col items-center justify-start pt-16">
     <ClientOnly>
-      <ColorModeToggle />
       <AudioPlayer />
     </ClientOnly>
   </div>
