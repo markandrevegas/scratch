9469bceeb59a844508f2429942e65f48f4481456
markandre <mark@haandvaerker.dk>
2025-08-23 12:02:04 +0200

    colormodetoggle fixes.

--- components/AudioPlayer.vue
+++ components/AudioPlayer.vue
@@ -35,22 +35,25 @@
 </script>
 <template>
   <div>
-    <div class="shadow-lg border border-slate-100 grid grid-cols-4 rounded-l-lg">
+    <div class="shadow-lg grid grid-cols-4 rounded-lg dark:bg-slate-200/30">
       <div class="flex justify-center items-center w-full">
         <NuxtImg v-if="song.art" provider="ipx" :src="song.art" class="h-32 w-32 rounded-l-lg shadow" />
         <NuxtImg v-else src="48x48" provider="placehold" class="w-full h-full rounded-l-lg shadow" />
       </div>
       <div class="col-span-3 rounded-lg flex justify-start items-start">
         <div class="flex flex-col justify-between h-full w-full pt-4 pr-4 pb-2 pl-4 gap-2">
-          <div>
-            <p class="text-sm font-semibold text-abyssal dark:text-slate-200 mb-[2px]">{{ song.title }}</p>
-            <p class="text-xs font-medium text-slate-600 opacity-60 dark:text-slate-300 leading-none">{{ song.artist }}</p>
+          <div class="flex justify-between items-start">
+            <div>
+              <p class="text-sm font-semibold text-abyssal dark:text-slate-300 mb-[2px]">{{ song.title }}</p>
+              <p class="text-xs font-medium text-slate-600 opacity-60 dark:text-slate-200 leading-none">{{ song.artist }}</p>
+            </div>
+            <ColorModeToggle />
           </div>
           <div class="w-full flex justify-between items-center">
             <progress :value="elapsedTime" max="3600" class="progress-bar w-5/6 h-[2px] rounded-full overflow-hidden appearance-none" />
-            <span class="text-xs text-slate-600/60">{{ formatTime(elapsedTime) }}</span>
+            <span class="leading-none text-xs text-slate-600/60 dark:text-slate-300">{{ formatTime(elapsedTime) }}</span>
           </div>
-          <div class="h-8 w-full grid grid-cols-4 text-abyssal hover:text-abyssal dark:hover:text-slate-200 rounded-full flex items-center justify-between hover:cursor-pointer duration-500" @click="isPlaying ? pause() : play()">
+          <div class="h-8 w-full grid grid-cols-4 text-abyssal dark:text-slate-300 hover:text-abyssal dark:hover:text-slate-200 rounded-full flex items-center justify-between hover:cursor-pointer duration-500" @click="isPlaying ? pause() : play()">
             <Transition name="fade" mode="out-in">
               <Icon name="jam:refresh-reverse" class="h-4 w-4"  />
             </Transition>
--- components/ColorModeToggle.vue
+++ components/ColorModeToggle.vue
@@ -1,8 +1,7 @@
 <template>
   <ClientOnly>
     <button
-      class="px-3 py-2"
-      :class="isDark ? 'bg-transparent text-neutral-300' : 'bg-transparent text-sky-950'"
+      :class="isDark ? 'bg-transparent text-slate-300' : 'bg-transparent text-abyssal'"
       @click="toggle"
       >
       <span v-if="isDark">
@@ -10,7 +9,7 @@
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
           fill="currentColor"
-          class="size-6"
+          class="w-4 h-4"
         >
           <path
             fill-rule="evenodd"
@@ -25,7 +24,7 @@
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
           fill="currentColor"
-          class="size-6"
+          class="w-4 h-4"
         >
           <path
             d="M12 2.25a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-1.5 0V3a.75.75 0 0 1 .75-.75ZM7.5 12a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM18.894 6.166a.75.75 0 0 0-1.06-1.06l-1.591 1.59a.75.75 0 1 0 1.06 1.061l1.591-1.59ZM21.75 12a.75.75 0 0 1-.75.75h-2.25a.75.75 0 0 1 0-1.5H21a.75.75 0 0 1 .75.75ZM17.834 18.894a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 1 0-1.061 1.06l1.59 1.591ZM12 18a.75.75 0 0 1 .75.75V21a.75.75 0 0 1-1.5 0v-2.25A.75.75 0 0 1 12 18ZM7.758 17.303a.75.75 0 0 0-1.061-1.06l-1.591 1.59a.75.75 0 0 0 1.06 1.061l1.591-1.59ZM6 12a.75.75 0 0 1-.75.75H3a.75.75 0 0 1 0-1.5h2.25A.75.75 0 0 1 6 12ZM6.697 7.757a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 0 0-1.061 1.06l1.59 1.591Z"
