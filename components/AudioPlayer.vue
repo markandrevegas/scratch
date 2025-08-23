e180a5c546976a711f73b71cb16df051ec634c3e
markandre <mark@haandvaerker.dk>
2025-08-23 19:30:14 +0200

    fixed elapsedTime bug

--- components/AudioPlayer.vue
+++ components/AudioPlayer.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-  import { onMounted, onUnmounted, watch } from 'vue'
+  import { onMounted } from 'vue'
   import { useRadio } from '../composables/useScratchRadio'
 
   const { isPlaying, play, pause, elapsedTime, song, fetchScratchRadio } = useRadio()
@@ -10,37 +10,18 @@
     return `${m}:${s}`
   }
 
-  let interval: number | null = null
-
-  watch(isPlaying, (playing) => {
-    if (playing) {
-      if (!interval) {
-        interval = window.setInterval(() => {
-          elapsedTime.value++
-        }, 1000)
-      }
-    } else {
-      if (interval) {
-        clearInterval(interval)
-        interval = null
-      }
-    }
-  })
   onMounted(async () => {
     fetchScratchRadio()
-  })
-  onUnmounted(() => {
-    if (interval) clearInterval(interval)
   })
 </script>
 <template>
   <div>
-    <div class="shadow-lg grid grid-cols-4 rounded-lg dark:bg-abyssal">
-      <div class="flex justify-center items-center w-full">
+    <div class="shadow-lg grid grid-cols-3 rounded-lg dark:bg-abyssal w-96">
+      <div class="col-span-1 flex justify-center items-center w-full">
         <NuxtImg v-if="song.art" provider="ipx" :src="song.art" class="h-32 w-32 rounded-l-lg shadow object-cover" />
-        <NuxtImg v-else src="48x48" provider="placehold" class="w-full h-full rounded-l-lg shadow" />
+        <NuxtImg v-else src="64x64" provider="placehold" class="h-32 w-32 rounded-l-lg shadow" />
       </div>
-      <div class="col-span-3 rounded-lg flex justify-start items-start">
+      <div class="col-span-2 rounded-lg flex justify-start items-start">
         <div class="flex flex-col justify-between h-full w-full pt-4 pr-4 pb-2 pl-4 gap-2">
           <div class="flex justify-between items-start">
             <div>
@@ -50,7 +31,7 @@
             <ColorModeToggle />
           </div>
           <div class="w-full flex justify-between items-center">
-            <progress :value="elapsedTime" max="3600" class="progress-bar w-5/6 h-[2px] rounded-full overflow-hidden appearance-none" />
+            <progress :value="elapsedTime" max="720" class="progress-bar w-5/6 h-[2px] rounded-full overflow-hidden appearance-none" />
             <span class="leading-none text-xs text-slate-600/60 dark:text-slate-300">{{ formatTime(elapsedTime) }}</span>
           </div>
           <div class="h-8 w-full grid grid-cols-4 text-abyssal dark:text-slate-300 hover:text-abyssal dark:hover:text-slate-200 rounded-full flex items-center justify-between hover:cursor-pointer duration-500" @click="isPlaying ? pause() : play()">
