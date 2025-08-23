0d403c6c340ee9d6299ec1c742399018d70f5317
markandre <mark@haandvaerker.dk>
2025-08-23 18:11:56 +0200

    prefetch urls

--- pages/ScratchRadio.vue
+++ pages/ScratchRadio.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="dark:bg-abyssal h-screen flex flex-col items-center justify-start pt-16">
+  <div class="dark:bg-slate-500 h-screen flex flex-col items-center justify-start pt-16">
     <ClientOnly>
       <AudioPlayer />
     </ClientOnly>
