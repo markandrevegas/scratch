0cae14bbdac32f06ce6d2092815a5b7a8b7363d0
Mark Stephens <mark@haandvaerker.dk>
2025-10-01 11:35:56 +0200

    image dimensions

--- components/AudioPlayer.vue
+++ components/AudioPlayer.vue
@@ -143,8 +143,8 @@ onMounted(async () => {
 </script>
 <template>
 	<div class="flex gap-4">
-    <div class="h-72 w-72 mx-auto flex flex-col rounded-t-lg">
-      <div class="h-48 relative flex flex-col justify-center items-center rounded-t-lg overflow-hidden">
+    <div class="h-96 w-72 mx-auto flex flex-col rounded-t-lg">
+      <div class="h-72 relative flex flex-col justify-center items-center rounded-t-lg overflow-hidden">
         <div class="absolute h-full w-full flex justify-center items-center overflow-hidden">
           <NuxtImg v-if="song.art" :src="song?.art || unsplashImage?.urls?.regular" provider="ipx" class="h-full w-full rounded-t-lg object-cover object-top" />
         </div>
@@ -189,7 +189,7 @@ onMounted(async () => {
         </div>
       </div>
     </div>
-    <div class="h-72 w-72 dark:bg-abyssal dark:text-zinc-100">
+    <div class="h-96 w-72 dark:bg-abyssal dark:text-zinc-100">
       <div v-if="favorites.length > 0" class="">
         <div class="px-2 pt-4 pb-3 pl-3 sticky top-0 z-20 flex justify-start items-center gap-2">
           <Icon name="jam-heart" class="size-3" />
