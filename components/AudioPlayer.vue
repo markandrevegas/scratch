62ea60c80a1501073b5efbfaa3832185db226014
Mark Stephens <mark@haandvaerker.dk>
2025-10-02 11:56:18 +0200

    height fix for player. edited unsplash query

--- components/AudioPlayer.vue
+++ components/AudioPlayer.vue
@@ -132,7 +132,7 @@ onMounted(async () => {
 })
 </script>
 <template>
-	<div class="flex items-center gap-4">
+	<div class="flex items-center gap-4 mt-8">
     <div class="h-96 w-72 mx-auto flex flex-col rounded-t-lg">
       <div class="h-72 relative flex flex-col justify-center items-center rounded-t-lg overflow-hidden">
         <div class="absolute h-full w-full flex justify-center items-center overflow-hidden">
