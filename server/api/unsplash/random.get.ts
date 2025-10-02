611e9babfb0403b285983a6f10175a00a4fad478
markandrevegss <markandrevega@me.com>
2025-10-02 08:32:00 +0200

    cleaned up unsplash and height of albumart and controls

--- server/api/unsplash/random.get.ts
+++ server/api/unsplash/random.get.ts
@@ -3,27 +3,26 @@ const UNSPLASH_KEY = process.env.NUXT_UNSPLASH_ACCESS_KEY
 const unsplashBase = process.env.NUXT_UNSPLASH_BASE || 'https://api.unsplash.com'
 
 export default defineEventHandler(async () => {
-  // const config = useRuntimeConfig() as RuntimeConfig
-  // Always use a fixed query for Unsplash
-  const params = new URLSearchParams({
-    query: '70s reggae',
-    orientation: 'landscape',
-    content_filter: 'high',
-    count: '1'
-  })
+	// Always use a fixed query for Unsplash
+	const params = new URLSearchParams({
+		query: "70s reggae",
+		orientation: "squarish",
+		content_filter: "high",
+		count: "1"
+	})
 
-  // Use Authorization header with runtime config
-  const res = await fetch(`${unsplashBase}/photos/random?${params.toString()}`, {
-    headers: {
-      Authorization: `Client-ID ${UNSPLASH_KEY}`
-    }
-  })
+	// Use Authorization header with runtime config
+	const res = await fetch(`${unsplashBase}/photos/random?${params.toString()}`, {
+		headers: {
+			Authorization: `Client-ID ${UNSPLASH_KEY}`
+		}
+	})
 
-  if (!res.ok) {
-    const errorText = await res.text()
-    throw new Error(`Unsplash fetch failed: ${res.status} ${errorText}`)
-  }
+	if (!res.ok) {
+		const errorText = await res.text()
+		throw new Error(`Unsplash fetch failed: ${res.status} ${errorText}`)
+	}
 
-  const data = await res.json()
-  return data
+	const data = await res.json()
+	return data
 })
